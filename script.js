f69c09e1d4a67553a89697b7d5337b9a42da4006
Tara <81445969+Tara-Q@users.noreply.github.com>
2021-09-07 12:37:54 +0100

    initial commit
    completed basic outline re parts 1-3 of level 100

--- script.js
+++ script.js
@@ -6,7 +6,53 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach(createACard);
 }
 
+function padNumber(n) {
+  return n.toString().padStart(2, "0");
+}
+
+function makeEpisodeNumber(episode) {
+  return `S${padNumber(episode.season)}E${padNumber(episode.number)}`;
+}
+
+function createACard(episode) {
+  
+  let card = document.createElement("div");
+  let root = document.querySelector("#root");
+  root.appendChild(card);
+
+  let episodeCode = makeEpisodeNumber(episode);
+  let h1 = document.createElement("h1");
+  h1.innerHTML = `${episode.name} - ${episodeCode}`;
+  
+
+  let cardSummary = document.createElement("span");
+  cardSummary.innerHTML = episode.summary;
+ 
+
+  let episodeImage = document.createElement("img");
+  episodeImage.src = episode.image.medium;
+  episodeImage.alt = "";
+
+  card.appendChild(h1);
+  card.appendChild(episodeImage);
+  card.appendChild(cardSummary);
+  return card;
+}
+// let credits = document.createElement("span");                       
+// let creditText = document.createTextNode("This site was created using data from TV Maze");  
+// let creditLink = document.createElement("a");
+
+// credits.appendChild(creditText); 
+// creditText.appendChild(creditLink);                                       
+// document.body.appendChild(credits);     
+
+// function layoutOneCard() {
+// let h1 = document.createElement("episodeHeader");
+
+// }
+
 window.onload = setup;
+
